bae7e43eaf3128fbf84dcdb7c593ce17ac8fa992
RLS1000 <ryan@lopezsolis.de>
2025-07-15 00:06:21 +0200

    Update bookingService.js

--- services/bookingService.js
+++ services/bookingService.js
@@ -1,7 +1,7 @@
 // services/bookingService.js
 const db = require('../db');
 const { sendMail } = require('./mailService');
-
+console.log("🚀 convertLeadToBooking gestartet mit:", { leadId, kontakt, rechnungsadresse });
 async function convertLeadToBooking({ leadId, kontakt, rechnungsadresse }) {
   // 1. Lead laden
   const leadResult = await db.query('SELECT * FROM lead WHERE id = $1', [leadId]);
