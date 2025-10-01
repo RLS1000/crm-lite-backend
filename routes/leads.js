899e353bdfe14dd951573dd9bdad7a02285edf9d
RLS1000 <ryan@lopezsolis.de>
2025-10-01 14:31:02 +0200

    Update leads.js

--- routes/leads.js
+++ routes/leads.js
@@ -67,7 +67,7 @@ router.post('/', async (req, res) => {
         $14, $15, $16, $17,
         $18, $19, $20, $21,
         $22, $23,
-        $24, $25, $26
+        $24, $25, $26, $27
       )`,
       [
         external_id, group_id, vorname, nachname, email, telefon,
